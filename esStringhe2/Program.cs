6194de3da9473b66884795c2cc36886cf4b52442
contidavide-sudo <conti.davide@franchettisalviani.net>
2026-02-02 00:26:05 +0100

    Aggiungere i file di progetto.

---
+++ esStringhe2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace esStringhe2
+{
+    internal class Program
+    {
+        static int Trasformazione(string num)
+        {
+            int numeroTras = 0, cont = 0;
+
+            for (int i = 7; i >= 0; i--)
+            {
+                if (num[i] == 1)
+                {
+                    numeroTras=numeroTras+2
+
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esercizio 1");
+
+            string numero;
+
+            Console.WriteLine("Inserisci il numero binario di 8 bit: ");
+            numero = Console.ReadLine();
+
+            Trasformazione(numero);
+
+        }
+    }
+}
